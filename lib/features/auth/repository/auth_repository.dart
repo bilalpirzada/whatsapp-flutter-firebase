9b8b99f8aaa06af95d7ad15c0addc1c0ee73a6f3
92306 <bilal.se74@gmail.com>
2022-07-22 16:35:25 +0500

    completed until sending and verifying OTP

---
+++ lib/features/auth/repository/auth_repository.dart
@@ -0,0 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_ui/common/utils/utils.dart';
+import 'package:whatsapp_ui/features/auth/screens/otp_screen.dart';
+import 'package:whatsapp_ui/features/auth/screens/user_information_screen.dart';
+
+final authRepositoryProvider = Provider(
+    (ref) => AuthRepository(FirebaseAuth.instance, FirebaseFirestore.instance));
+
+class AuthRepository {
+  final FirebaseAuth auth;
+  final FirebaseFirestore firestore;
+
+  AuthRepository(this.auth, this.firestore);
+
+  void signInWithPhone(BuildContext context, String phoneNumber) async {
+    try {
+      await auth.verifyPhoneNumber(
+          phoneNumber: phoneNumber,
+          verificationCompleted: (PhoneAuthCredential credential) async {
+            await auth.signInWithCredential(credential);
+          },
+          verificationFailed: (e) {
+            throw Exception(e.message);
+          },
+          codeSent: ((String verficationId, int? resendToken) async {
+            Navigator.pushNamed(context, OTPScreen.routeName,
+                arguments: verficationId);
+          }),
+          codeAutoRetrievalTimeout: (String verficationId) {});
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context: context, content: e.message!);
+    }
+  }
+
+  void verifyOTP(
+      {required BuildContext context,
+      required String verificationId,
+      required String userOTP}) async {
+    try {
+      PhoneAuthCredential credential = PhoneAuthProvider.credential(
+          verificationId: verificationId, smsCode: userOTP);
+      await auth.signInWithCredential(credential);
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        UserInformationScreen.routeName,
+        (route) => false,
+      );
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context: context, content: e.message!);
+    }
+  }
+}
