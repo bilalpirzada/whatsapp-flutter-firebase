0a9149d8a73bc324761feafa201e958cd015c6ea
Muhammad Bilal <bilal.se74@gmail.com>
2022-07-31 10:35:23 +0500

    changing online and offline status

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -112,4 +112,11 @@ class AuthRepository {
         .snapshots()
         .map((event) => UserModel.fromMap(event.data()!));
   }
+
+  void setUserState(bool isOnline) async {
+    await firestore
+        .collection('users')
+        .doc(auth.currentUser!.uid)
+        .update({'isOnline': isOnline});
+  }
 }
